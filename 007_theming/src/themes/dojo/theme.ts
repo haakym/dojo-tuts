94c2332ac9032ec77c08d3e13046e0429eabcf73
Dominic <haakym@gmail.com>
2018-03-19 14:51:04 +0000

    007_theming all done

--- 007_theming/src/themes/dojo/theme.ts
+++ 007_theming/src/themes/dojo/theme.ts
@@ -1,9 +1,6 @@
-import * as textInput from './@dojo/widgets/text-input/text-input.m.css';
-import * as button from './@dojo/widgets/button/button.m.css';
 import * as worker from './worker.m.css';
-
+import * as workerForm from './workerForm.m.css';
 export default {
-	'@dojo/widgets/button': button,
-	'@dojo/widgets/text-input': textInput,
-	'biz-e-corp/worker': worker
+	'biz-e-corp/worker': worker,
+	'biz-e-corp/workerForm': workerForm
 };
