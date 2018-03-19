70c2106ed0af3d67539fd5c89dd287d2c6913bcd
Dominic <haakym@gmail.com>
2018-03-19 14:44:08 +0000

    007_theming #2 done

---
+++ 007_theming/src/themes/dojo/theme.ts
@@ -0,0 +1,9 @@
+import * as textInput from './@dojo/widgets/text-input/text-input.m.css';
+import * as button from './@dojo/widgets/button/button.m.css';
+import * as worker from './worker.m.css';
+
+export default {
+	'@dojo/widgets/button': button,
+	'@dojo/widgets/text-input': textInput,
+	'biz-e-corp/worker': worker
+};
